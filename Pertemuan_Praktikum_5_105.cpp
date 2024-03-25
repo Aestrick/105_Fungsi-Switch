9c6411938ac950ded14f2f3167de5a892a711062
xerro <nashrul.fikri19@gmail.com>
2024-03-25 10:37:46 +0700

    Add project files.

---
+++ Pertemuan_Praktikum_5_105.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+int a, b;
+void input()
+{
+	cout << "Bilangan  pertama = ";
+	cin >> a;
+
+	cout << "Bilangan kedua = ";
+		cin >> b;
+}
+
+int penjumlahan(int bil_1, int bil_2) {
+	return bil_1 + bil_2;
+}
+
+int pengurangan(int bil_1, int bil_2) {
+	return bil_1 - bil_2;
+}
+int perkalian(int bil_1, int bil_2) {
+	return bil_1 * bil_2;
+}
+int pembagian(int bil_1, int bil_2) {
+	return bil_1 / bil_2;
+}
+
+
+
+
+
+int main()
+{
+	int pilihan;
+
+	do {
+		system("cls");
+		cout << "======== Menu ========" << endl;
+		cout << "1.penjumlahan" << endl;
+		cout << "2.pengurangan" << endl;
+		cout << "3.perkalian" << endl;
+		cout << "4.pembagian" << endl;
+		cout << "5.keluar" << endl;
+		cout << "Masukkan menu yang ingin dipilih (1/2/3/4/5): ";
+		cin >> pilihan;
+
+		switch (pilihan)
+		{
+		case 1:
+			input();
+			cout << "Hasil penjumlahan = " << penjumlahan(a, b) << endl;
+			system("pause");
+			break;
+		case 2:
+			input();
+			cout << "Hasil pengurangan = " << pengurangan(a, b) << endl;
+			system("pause");
+			break;
+		case 3:
+			input();
+			cout << "Hasil perkalian = " << perkalian(a, b) << endl;
+			system("pause");
+			break;
+		case 4:
+			input();
+			cout << "Hasil pembagian = " << pembagian(a, b) << endl;
+			system("pause");
+			break;
+		case 5:
+			cout << "menu tidak tersedia silahkan pilih lagi !!!" << endl;
+			break;
+		}
+
+	} while (pilihan != 5);
+
+}
